6b5474d72e565287a60c2d407248adec07d97e29
Ross King <romeokilo125@gmail.com>
2018-03-25 23:16:35 -0400

    minimum playable game. styling to come.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,58 +9,82 @@ function game() {
   let wins = 0;
   let losses = 0;
   let wordComplete = false;
-
-
+  let alphaRe = /[a-z]/;
 
   function StartGame() {
     secretWord = words[Math.floor(Math.random() * words.length)];
+    blanksArray = []
     guessesLeft = secretWord.length + 3;
     for (i = 0; i < secretWord.length; i++) {
-      if (secretWord[i].match(/\w/)) {
+      if (alphaRe.test(secretWord[i]) === true) {
         blanksArray.push("_ ");
+      } else if (secretWord[i] === " ") {
+        blanksArray.push("&nbsp; ");
       } else {
         blanksArray.push(secretWord[i] + " ");
       }
+      document.querySelector("#resultsBox").innerHTML = "";
     }
-    console.log(secretWord);
+    console.log("SecretWord: " + secretWord);
     guessedLetters = [];
     wordComplete = false;
     document.querySelector("#wordBox").innerHTML = blanksArray.join('');
+    document.querySelector('#guessedList').innerHTML = guessedLetters.toString();
+    document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+
+  }
+
+  function WinGame() {
+    wins++;
+    document.querySelector('#resultsBox').innerHTML = "<h1>Congratulations!</h1>\n<h3>The word was " + secretWord + "</h3>\n<p>Press ENTER to play again.</p>";
+    document.querySelector('#wins').innerHTML = wins;
+  }
+
+  function LoseGame() {
+    losses++;
+    document.querySelector('#resultsBox').innerHTML = "<h1>I'm Sorry, you lost!</h1>\n<h3>The word was " + secretWord + "</h3>\n<p>Press ENTER to play again.</p>";
+    document.querySelector('#losses').innerHTML = losses;
   }
 
   document.onkeyup = function(event) {
     let guess = event.key.toLowerCase();
-    if (guess === "ENTER") {
+    if (guess === "enter" && (guessesLeft <= 0 || wordComplete === true)) {
       StartGame();
+      return;
+    }
+    if (alphaRe.test(guess) === false) {
+      alert("What kind of trickery are you trying to pull? That's not a letter!")
       return;
     }
     if (guessesLeft <= 0 || wordComplete === true || guessedLetters.indexOf(guess) != -1) {
       return;
     }
-    console.log("Guess: " + guess);
     guessedLetters.push(guess);
-    if (secretWord.indexOf(guess) != -1) {
+    console.log("Guess: " + guess);
 
+    guessesLeft--;
+    document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+    document.querySelector('#guessedList').innerHTML = guessedLetters.toString();
 
-
-      document.querySelector('#resultsBox').innerHTML = "<h1>Congratulations!</h1>\n<h3>The letter was " + secretLetter + "</h3>\n<p>Press ENTER to play again.</p>";
-      document.querySelector('#wins').innerHTML = wins;
-    } else {
-      document.querySelector('#resultsBox').innerHTML = "<h1>Try Again!</h1>\n<h3>The letter is still unknown.</h3>"
-      document.querySelector('#guessedList').innerHTML = guessedLetters.toString();
-      guessesLeft--;
-      if (guessesLeft <= 0) {
-        losses++;
-        document.querySelector('#resultsBox').innerHTML = "<h1>I'm Sorry, you lost!</h1>\n<h3>The letter was " + secretLetter + "</h3>\n<p>Press ENTER to play again.</p>";
-        document.querySelector('#losses').innerHTML = losses;
+    if (RegExp(guess).test(secretWord)) {
+      secretWord.split('').forEach(function(elt, i) {
+        if (elt === guess) {
+          blanksArray[i] = guess;
+          document.querySelector("#wordBox").innerHTML = blanksArray.join('');
+        }
+      });
+      if (RegExp('_').test(blanksArray.join('')) === false) {
+        wordComplete = true;
+        WinGame()
+        return;
       }
-      document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+    } else {
+      if (guessesLeft <= 0) {
+        LoseGame();
+        return;
+      }
     }
   }
-
-
-
-
 
   StartGame();
 }
